10f9845ca106c5f7ccfbc3d3eba3746901c21e23
woonohyo <woonohyo@nhnnext.org>
2014-05-26 20:12:56 +0900

    trim ServerInitializer

--- woonoSMTP/src/server/ServerInitializer.java
+++ woonoSMTP/src/server/ServerInitializer.java
@@ -31,69 +31,6 @@ public class ServerInitializer implements Runnable {
 		}
 	}
 
-	private void handleClient(Socket socket) {
-		System.out.println("요청을 처리합니다.");
-		try {
-			OutputStream os = socket.getOutputStream();
-			InputStream is = socket.getInputStream();
-			byte[] buffer = new byte[4];
-			String clientData;
-
-			// 클라이언트에게 서비스가 준비됨을 알리는 220 코드 전송.
-			serviceReady220(os);
-
-			while (is.read(buffer) != -1) {
-				clientData = new String(buffer);
-				System.out.println("clientData: " + clientData);
-			}
-
-			socket.close();
-
-			/*
-			 * 
-			 * // 클라이언트의 HELO 명령 읽은 뒤, // 무사히 완료했음을 알리는 250 코드 전송.
-			 * readClientData(is, buffer); requestComplete250(os);
-			 * 
-			 * // 클라이언트의 MAIL FROM 명령을 읽은 뒤, // 무사히 완료했음을 알리는 250 코드 전송.
-			 * readClientData(is, buffer); requestComplete250(os);
-			 * 
-			 * // 클라이언트의 RCPT 명령을 읽은 뒤, // 무사히 완료했음을 알리는 250 코드 전송.
-			 * readClientData(is, buffer); requestComplete250(os);
-			 * 
-			 * // 클라이언트의 DATA 명령을 읽은 뒤, // 본문 전송을 요구하는 354 코드 전송.
-			 * readClientData(is, buffer); startMailInput354(os);
-			 * 
-			 * // 클라이언트의 본문 내용을 읽은 뒤, // 무사히 완료했음을 알리는 250 코드 전송.
-			 * readClientData(is, buffer); requestComplete250(os);
-			 * 
-			 * readClientData(is, buffer); readClientData(is, buffer);
-			 */
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	private void startMailInput354(OutputStream os) throws IOException {
-		String message = "354\n";
-		os.write(message.getBytes());
-	}
-
-	private void requestComplete250(OutputStream os) throws IOException {
-		String message = "250\n";
-		os.write(message.getBytes());
-	}
-
-	private void readClientData(InputStream is, byte[] buffer)
-			throws IOException {
-		is.read(buffer);
-		String fromClient = new String(buffer);
-		System.out.println(fromClient);
-
-	}
-
 	private void serviceReady220(OutputStream os) throws IOException {
 		String message = "220 Woonohyo SMTP Service Ready\n";
 		os.write(message.getBytes());
